bd0357dd7c02bc9565ad910b5db3a82ca7a10593
Juan Ahumada <juan.ahumada94@gmail.com>
2022-06-02 20:20:09 -0500

    Trabajando con el AddFragment.kt - Configuraciones previas - Inflate con Binding - Constante Gallery - Configuracion de botones - Creaciones de funciones vacias postPhoto y openGalley

--- app/src/main/java/com/example/socialimagesnetwork/AddFragment.kt
+++ app/src/main/java/com/example/socialimagesnetwork/AddFragment.kt
@@ -1,17 +1,46 @@
 package com.example.socialimagesnetwork
 
+import android.net.Uri
+import android.os.Binder
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.socialimagesnetwork.databinding.FragmentAddBinding
 
 class AddFragment : Fragment() {
+
+    private val RC_GALLERY = 18
+
+    //Enlace
+    private lateinit var mBinding: FragmentAddBinding
+
+    private var mPhotoSelectedUri : Uri? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        //Inflar con Binding
+        mBinding = FragmentAddBinding.inflate(inflater,container, false)
+        return mBinding.root
+    }
+
+    // Configurar Botones
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mBinding.btnPost.setOnClickListener{ postPhoto() }
+
+        mBinding.btnSelect.setOnClickListener{ openGallery() }
+    }
+
+    private fun postPhoto(){
+
+    }
+
+    private fun openGallery(){
+
     }
 }
